7143af561868f366e5e3ae900169578e6b1775b8
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-10-30 16:25:14 +0530

    Firebase Deploy

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcT6ki318NwLqcrvvKDQh8omohwK-XnwQ",
+  authDomain: "covid-19-project-77c24.firebaseapp.com",
+  databaseURL: "https://covid-19-project-77c24.firebaseio.com",
+  projectId: "covid-19-project-77c24",
+  storageBucket: "covid-19-project-77c24.appspot.com",
+  messagingSenderId: "206247767998",
+  appId: "1:206247767998:web:48964ee3c5fc47c3d653ef",
+  measurementId: "G-28TR03CMJ3",
+};
+
+firebase.initializeApp(firebaseConfig);
