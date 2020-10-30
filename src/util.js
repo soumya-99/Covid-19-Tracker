b0e05ef0143d3207a362cfaf67ede71ffe813578
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-10-30 13:26:10 +0530

    Tooltips with Flag Done

--- src/util.js
+++ src/util.js
@@ -54,7 +54,24 @@ export const showDataOnMap = (data, casesType = "cases") =>
       }
     >
       <Popup>
-        <h1>I"M A Popup</h1>
+        {/* <h1>I"M A Popup</h1> */}
+
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          />
+          <div className="info-name"> {country.country} </div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
       </Popup>
     </Circle>
   ));
